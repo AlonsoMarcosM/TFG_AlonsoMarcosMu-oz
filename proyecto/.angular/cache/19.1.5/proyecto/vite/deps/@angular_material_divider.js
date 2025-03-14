b339eba0ac15d8a5311b11e70f50a5e09cf63bd6
Alonso <alonso.marcos@alu.uclm.es>
2025-03-14 13:34:24 +0100

    Añadido backend al monorepo

---
+++ proyecto/.angular/cache/19.1.5/proyecto/vite/deps/@angular_material_divider.js
@@ -0,0 +1,17 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-B6WKO3U2.js";
+import "./chunk-4F3FZADF.js";
+import "./chunk-3KS3D65X.js";
+import "./chunk-WMPGM56R.js";
+import "./chunk-FXJEPELR.js";
+import "./chunk-556KATUP.js";
+import "./chunk-IYE42FHU.js";
+import "./chunk-LBBSG2YE.js";
+import "./chunk-WSXI74FV.js";
+import "./chunk-NGNUV6BG.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
